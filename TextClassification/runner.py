2d1ba46c7784c2df7f3f09a6ad4bcd649b76435e
snowcxt <snowcxt@gmail.com>
2017-09-29 14:42:47 -0700

    dump categories.pkl

--- TextClassification/runner.py
+++ TextClassification/runner.py
@@ -3,27 +3,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 vectorizer = joblib.load('./vectorizer.pkl')
 clf = joblib.load('./MultinomialNB.pkl')
-categories = [
-    'alt.atheism',
-    'comp.graphics',
-    'comp.os.ms-windows.misc',
-    'comp.sys.ibm.pc.hardware',
-    'comp.sys.mac.hardware',
-    'comp.windows.x',
-    'misc.forsale',
-    'rec.autos',
-    'rec.motorcycles',
-    'rec.sport.baseball',
-    'rec.sport.hockey',
-    'sci.crypt',
-    'sci.electronics',
-    'sci.med',
-    'sci.space',
-    'soc.religion.christian',
-    'talk.politics.guns',
-    'talk.politics.mideast',
-    'talk.politics.misc',
-    'talk.religion.misc']
+categories = joblib.load('./categories.pkl')
 
 def predict(text):  
     pred = clf.predict(vectorizer.transform([text]))
